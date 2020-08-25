99343fdfe7ff0399132e584094ca79cb6bf8714f
xiaozheng <1067652489@qq.com>
2020-08-25 16:53:48 +0800

    v1

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     //
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = null;
 
     public function rotation_show() {
         return $this->hasOne(RotationShow::class,'image_id');
