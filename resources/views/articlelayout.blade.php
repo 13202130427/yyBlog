62917fa684ba4903fda1ce6902a9e8b6325be6f8
xiaozheng <1067652489@qq.com>
2020-10-04 17:21:39 +0800

    fix bug

--- resources/views/articlelayout.blade.php
+++ resources/views/articlelayout.blade.php
@@ -10,15 +10,33 @@
                     <li class="active">@yield('title')</li>
                     <span class="w_navbar_tip">我们长路漫漫，只因学无止境。</span>
                 </ol>
-                <div class="money-alert" style="width: 100%;height: 400px;position: fixed;top:10%;left:30%;z-index: 999;" >
+                <div class="money-alert" id="money-alert" style="width: 100%;height: 400px;position: fixed;top:10%;left:30%;z-index: 999;" hidden = "hidden">
                     <div style="display:block;width: 50%;height: 600px;">
                         <div style="text-align: center;width:500px;height:500px;background-color: #fff;border-radius: 10px;border: 2px solid #bfbfbf; position: relative;">
-                            <div style="">
-                                <div>logo</div>
-                                <div>感谢你的支持，我会继续努力！</div>
-                                <div>码</div>
+                            <div style="display: inline-block;width: 100%;">
+                                <div style="margin-top: 50px;">
+                                    <img src="/img/log1.png" style="width: 120px;height: 40px;">
+                                </div>
+                                <div style="margin-top: 10px;color: #aaaaaa">感谢你的支持，我会继续努力！</div>
+                                <div style="margin-top: 40px;">
+                                    <div id="qrcode">
+                                        <img src="/img/getmoney-wx.png" style="width: 180px;height: 180px;">
+                                    </div>
+                                    <div style="display: inline-block;margin-top: 20px;height: 60px;position: relative;">
+                                        <div  style="position: absolute;left: 50px;">
+                                            <img src="/img/zfb.png" style="width: 50px;height: 50px;background-color: #f1f1f1;border-radius: 10px;" onclick="changeZFB()">
+                                            <input type="radio" name="zf" value="1"/>
+                                        </div>
+                                        <div style="position: absolute;right: 50px;">
+                                            <img src="/img/wx.png" style="width: 50px;height: 50px;" onclick="changeWX()">
+                                            <input type="radio" name="zf" checked value="2"/>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div style="position: absolute;right:20px;top: 20px;">x</div>
+                            <div style="position: absolute;right:20px;top: 20px;">
+                                <img src="/img/close.png" style="width: 20px;height: 20px;" onclick="closeAlert()">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -48,7 +66,26 @@
                     </div>
                     <script>
                         function getMoney() {
-
+                            $("#money-alert").prop("hidden",false);
+                        }
+                        function closeAlert() {
+                            $("#money-alert").prop("hidden",true);
+                        }
+                        $("input:radio[name='zf']").change(function () {
+                            var value = $("input:radio[name='zf']:checked").val();
+                            if(value == 1) {
+                                $("#qrcode").html("<img src=\"/img/getmoney-zfb.png\" style=\"width: 180px;height: 180px;\">");
+                            } else {
+                                $("#qrcode").html("<img src=\"/img/getmoney-wx.png\" style=\"width: 180px;height: 180px;\">");
+                            }
+                        })
+                        function changeZFB() {
+                            $("input:radio[name='zf']")[0].checked = true
+                            $("#qrcode").html("<img src=\"/img/getmoney-zfb.png\" style=\"width: 180px;height: 180px;\">");
+                        }
+                        function changeWX() {
+                            $("input:radio[name='zf']")[1].checked = true
+                            $("#qrcode").html("<img src=\"/img/getmoney-wx.png\" style=\"width: 180px;height: 180px;\">");
                         }
                     </script>
                 </div>
