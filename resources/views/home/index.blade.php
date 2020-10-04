802ebb3eb447ed627e65cc70c1e3566479266ce7
xiaozheng <1067652489@qq.com>
2020-10-04 17:48:56 +0800

    删除json

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -158,7 +158,7 @@
 
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <h3 class="panel-title">打钱爱你</h3>
+                            <h3 class="panel-title">打钱爱你哦</h3>
                         </div>
                         <div class="panel-body">
                             <img src="/img/getmoney-wx.jpg" style="height: 300.5px;width: 230.5px;" />
